ddf3b19fa506ce8600c318fda39efc3f19e44cec
HarisA <haris@comparables.ai>
2024-03-30 08:37:01 +0200

    adding Copy Dto and model

---
+++ src/main/java/fi/epassi/recruitment/copy/CopyDto.java
@@ -0,0 +1,32 @@
+package fi.epassi.recruitment.copy;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CopyDto {
+
+    @NotNull
+    private UUID isbn;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+
+    @NotNull
+    @DecimalMin(value = "1", message = "Quantity must be more than 0")
+    private Long copies;
+
+}
---
+++ src/main/java/fi/epassi/recruitment/copy/CopyModel.java
@@ -0,0 +1,41 @@
+package fi.epassi.recruitment.copy;
+
+import fi.epassi.recruitment.book.BookModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static java.sql.Types.VARCHAR;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "Copies")
+public class CopyModel {
+
+    @Id
+    @Column(name = "id")
+    private UUID isbn;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+
+    @NotNull
+    @DecimalMin(value = "1", message = "Quantity must be more than 1")
+    private Long copies;
+
+    @OneToOne(mappedBy = "copy")
+    private BookModel book;
+}
